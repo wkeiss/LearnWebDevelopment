38491cfb0c5f63406bd11107f102beef83ea1cbb
Cindy <1593006160@qq.com>
2020-11-06 22:29:37 +0800

    update

--- css_challenge/css_challenge7.js
+++ css_challenge/css_challenge7.js
@@ -1,12 +1,12 @@
 function mobiNav() {
-	var nav = document.getElementById('nav');
-	if (nav.className === 'topnav') {
-		nav.className += ' responsive';
-	}else{
-		nav.className = "topnav"
-	}
+  var nav = document.getElementById("nav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "topnav";
+  }
 }
 
-navIcon = document.getElementById('nav-icon')
+navIcon = document.getElementById("nav-icon");
 
-navIcon.onclick = mobiNav
+navIcon.onclick = mobiNav;
