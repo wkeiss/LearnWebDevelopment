5c4de67138109d9db47444a858e68116bc9df367
Cindy <1593006160@qq.com>
2020-11-09 14:12:30 +0800

    add image slide feature

--- css_challenge/css_challenge7.js
+++ css_challenge/css_challenge7.js
@@ -23,6 +23,7 @@ function changeOccupy(){
 
 //setInterval(changeOccupy, 2000);
 /*
+// add mouseover & mouseout event listeners to latestwork items
 let latestworkContainer = document.querySelector("#latestwork .container");
 let overlayStyle = document.querySelector(".item div.overlay").style;
 latestworkContainer.addEventListener("mouseover", function(e){
@@ -40,31 +41,55 @@ latestworkContainer.addEventListener("mouseout", function(e){
 	}
 })
 */
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+//add motal images effect to all demo <a> tag
+let demoLinks = document.querySelectorAll(".imgLink");
+//Get the slideshow container
+let slideshowContainer = document.querySelector(".slideshow-container")
+//get the slide image tag
+let slideImg = document.querySelector(".slideshow-container img")
+//add clickevent Listeners to all demoLinks for tiggering the image slides
+demoLinks.forEach(
+  function(element){
+    element.addEventListener("click", function(e){
+      e.preventDefault(); 
+      slideshowContainer.style.display = "block";
+      //make the iamge slides show the clicked item's image
+      slideImg.src = e.target.href;
+    })
+  })
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+//Next/previous controls
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+let nextBtn = document.querySelector(".next");
+let prevBtn = document.querySelector(".prev");
+let items = ["item-1", "item-2", "item-3", "item-4", "item-5", "item-6"]
+function showslideImg(i){
+  if(i < 0){
+    slideImg.src = `../images/portfolio/item-${items.length-1}.jpg`
+  }else if(i > (items.length + 1)){
+    slideImg.src = `../images/portfolio/${items[0]}.jpg`
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slideImg.src = `../images/portfolio/item-${i}.jpg`
 }
+
+nextBtn.addEventListener("click", function(){
+  i += 1;
+  showslideImg(i);
+});
+
+prevBtn.addEventListener("click", function(){
+  i -= 1;
+  showslideImg(i);
+});
+
+// Get the <span> element that closes the modal
+let closeBtns = document.querySelectorAll(".close");
+
+// When the user clicks on <span> (x), close the modal
+closeBtns.forEach(
+  function(element){
+    element.addEventListener("click", function(){
+      slideshowContainer.style.display = "none";
+    })
+  });
