b4ee9b202bba646cd185b49c59986a7b190a3f97
Cindy <1593006160@qq.com>
2020-11-11 22:47:31 +0800

    update

--- lonelyIsland/main.js
+++ lonelyIsland/main.js
@@ -1,10 +1,26 @@
 let textInput = document.querySelector("textarea#input");
 let textInputHeight;
+
 textInput.addEventListener("keydown", function (e) {
 	if(textInput.value === ""){
 		textInputHeight = 47;
 	}else if(e.code === "Enter"){
 		textInputHeight += 25;
 	}
-	textInput.style.height = textInputHeight + "px";
+	textInput.style.height = textInputHeight + "px";		
+})
+
+
+let postBtn = document.querySelector("#postBtn");
+postBtn.addEventListener("click", function(){
+	content = textInput.value;
+	textInput.value = "";
+	let itemDiv = document.createElement("div");
+	itemDiv.setAttribute("class", "item");
+	let postSection = document.querySelector("#post");
+	postSection.appendChild(itemDiv);
+	let para = document.createElement("p");
+	para.setAttribute("class", "content");
+	itemDiv.appendChild(para);
+	para.textContent = content;
 })
