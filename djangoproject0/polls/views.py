99e0776af656e735c29a3f4d4092c9799efb7e3e
Cindy <1593006160@qq.com>
2020-11-16 21:07:17 +0800

    add Django practice project

--- djangoproject0/polls/views.py
+++ djangoproject0/polls/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('<html><h1>How r you</h1></html>')
+    return HttpResponse("Hello, world. You're at the polls index.")
